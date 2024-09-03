7d244a3728fb83f1ccdb49bb733eb6da348f7a5a
itguytrades <itguytrades@proton.me>
2024-09-02 18:00:45 -0700

    added transfer and Buy funtions

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,6 +1,18 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
+
 contract Crowdsale {
-    string public name = "Crowdsale";
+    Token public token;
+
+
+    constructor(Token _token) {
+        token = _token;
+
+    }
+
+    function buyTokens(uint256 _amount) public {
+        token.transfer(msg.sender, _amount);
+    }
 }
--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -5,23 +5,54 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+const ether = tokens
+
 describe('Crowdsale', () => {
-  let crowdsale
+  let crowdsale, token
+  let accounts, deployer, user1
+
 	beforeEach(async () => {
       const Crowdsale = await ethers.getContractFactory('Crowdsale')
       const Token = await ethers.getContractFactory('Token')
 
       token = await Token.deploy('Dapp University', 'DAPP', '1000000')
 
-	  crowdsale = await Crowdsale.deploy(token.address) 
+      accounts = await ethers.getSigners()
+      deployer = accounts[0]
+      user1 = accounts[1]
+
+	  crowdsale = await Crowdsale.deploy(token.address)
+
+	  let transaction = await token.connect(deployer).transfer(crowdsale.address, tokens(1000000))
+      await transaction.wait()
 	})
 
 	describe('Deployment', () => {
-		it('has correct name', async () => {
-			expect(await crowdsale.name()).to.equal("Crowdsale")
+	  it('sends tokens to the Crowdsale contract', async () => {
+      expect(await token.balanceOf(crowdsale.address)).to.equal(tokens(1000000))
+      })
+      it('returns token address', async () => {
+  	  expect(await crowdsale.token()).to.equal(token.address)
+	  })
+	})
+
+	describe('Buying Tokens', () => {
+        let transaction, result
+        let amount = tokens(10)
+
+
+		describe('Success', () => {
+
+		  beforeEach(async () => {
+		    let transaction = await crowdsale.connect(user1).buyTokens(amount)
+	        let result = await transaction.wait()
+		  })
+			it('transfers tokens', async () => {
+
+		        expect(await token.balanceOf(crowdsale.address)).to.equal(tokens(999990))
+      			expect(await token.balanceOf(user1.address)).to.equal(amount)
+	   		})
 		})
-		it('returns token address', async () => {
-      		expect(await crowdsale.token()).to.equal(token.address)
- 	    })
+
 	})
 })
